124597f0de44820799ee66d05985c7fd8bd1e57a
Nekosan10 <bhmi2270073@gn.iwasaki.ac.jp>
2020-10-05 13:56:50 +0900

    AppBarのボタン機能だけ追加

---
+++ lib/screen/mainmenu.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:alarm_clock/val/string.dart';
+
+class MainMenu extends StatefulWidget{
+
+  MainMenu({Key key}):super(key:key);
+  @override
+  _MainMenuState createState()=>_MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu>{
+  String text='ホーム画面';
+  String yotei='ListViewを置いて、設定済みのアラームを表示する';
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      //AppBarはアプリのタイトルとかを表示してくれる領域のこと
+      appBar:AppBar(
+        title:Text(title),
+        actions: <Widget>[
+          //アイコンをそのままボタンにしてくれるWidget
+          //アイコン一覧は↓
+          //https://api.flutter.dev/flutter/material/Icons-class.html
+          IconButton(
+            icon:Icon(Icons.alarm_add),
+            //onPressed そのまんま押された時の動作を宣言するとこ
+            //setState 値を変更して画面を更新するよみたいな感じ
+            //画面の更新をかけないと表示上の数値は変わらない
+            onPressed:()=>setState((){
+              text='アラーム追加画面へ遷移する';
+            }),//
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: ()=>setState((){
+              text='設定画面に遷移する';
+          }),)
+        ],
+      ),
+      //body アプリのメイン画面
+      //Column 子供になるパーツが全部縦に並んでくれる　子供はchildren にいれる
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(text),
+          Text(yotei),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/val/color.dart
@@ -0,0 +1,6 @@
+//アプリ内で共通の使用色はここに宣言しておく
+//ボタンの色とか　文字の色とか
+//final Color 名前 =Colors.blue;
+// -> https://api.flutter.dev/flutter/material/Colors-class.html
+//final Color.fromRGBO(255,255,255,0);
+// -> RGBと透明度で指定するタイプ
