5b4b6f59ccb53d577be5ff6680fccccae770c486
Nekosan10 <bhmi2270073@gn.iwasaki.ac.jp>
2020-12-15 09:52:09 +0900

    曜日の処理の修正と無駄な処理を省いた

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,10 @@ Future<void> main() async {
   IsolateNameServer.removePortNameMapping(sendPortName);
   IsolateNameServer.registerPortWithName(receivePort.sendPort, sendPortName);
 
+  //set timezone
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation('Asia/Tokyo'));
+
 // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
   Workmanager.initialize(callbackDispatcher, isInDebugMode: false);
 
@@ -95,9 +99,6 @@ Future<void> main() async {
     BuildContext context;
     Navigator.pushNamedAndRemoveUntil(context, "/", (_) => false);
   });
-
-  tz.initializeTimeZones();
-  tz.setLocalLocation(tz.getLocation('Asia/Tokyo'));
 
   runApp(MyApp());
 }
